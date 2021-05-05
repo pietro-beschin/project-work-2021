e3892f3dc32dbf2bfacfd1dc34766e5229465451
yungfox <crifox12@gmail.com>
2021-05-05 11:28:07 +0200

    prima versione applicativo uffici

---
+++ c#/Applicazione Uffici/frmControlli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Applicazione_Uffici
+{
+    public partial class frmControlli : Form
+    {
+        public frmControlli()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ c#/Applicazione Uffici/frmMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Applicazione_Uffici
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayForm(Form myForm)
+        {
+            myForm.MdiParent = this;
+            myForm.FormBorderStyle = FormBorderStyle.None;
+            myForm.ControlBox = false;
+            myForm.MaximizeBox = false;
+            myForm.MinimizeBox = false;
+            myForm.ShowIcon = false;
+            myForm.Text = "";
+            myForm.Dock = DockStyle.Fill;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Form myForm = new frmCommesse();
+            DisplayForm(myForm);
+            myForm.Show();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Form myForm = new frmControlli();
+            DisplayForm(myForm);
+            myForm.Show();
+        }
+    }
+}
