f85726a2870f3a3015a5d9ea6cbbb2c2b007ce6d
yungfox <crifox12@gmail.com>
2021-05-11 17:38:29 +0200

    implementato tema dashboard

--- web-server/frontend/dati.js
+++ web-server/frontend/dati.js
@@ -11,63 +11,13 @@ const fetchFilteredData = () => {
         let nomeArticolo = $("#nome-articolo").val();
         let startDate = $("#dtp-inizio").val();
         let endDate = $("#dtp-fine").val();
-
-        console.log(nomeArticolo)
-        console.log(startDate)
-        console.log(endDate)
-
-        if(!nomeArticolo && !startDate && !endDate) {
-            event.preventDefault();
-            fetchAllData();
-        };
-        if(!nomeArticolo) {
-            event.preventDefault();
-            $.ajax({
-                type: 'GET',
-                url: `http://localhost:3000/api/history?from=${startDate}&to=${endDate}`,
-            }).then(result => {
-                $('#accordion-commesse').empty();
-                result.forEach(commessa => addCommessaToList(commessa));
-            });
-        }
-        if(!startDate && !endDate) {
-            event.preventDefault();
-            $.ajax({
-                type: 'GET',
-                url: `http://localhost:3000/api/history?articolo=${nomeArticolo}`,
-            }).then(result => {
-                $('#accordion-commesse').empty();
-                result.forEach(commessa => addCommessaToList(commessa));
-            });
-        }
-        if(nomeArticolo && startDate && endDate) {
-            event.preventDefault();
-            $.ajax({
-                type: 'GET',
-                url: `http://localhost:3000/api/history?articolo=${nomeArticolo}&from=${startDate}&to=${endDate}`,
-            }).then(result => {
-                $('#accordion-commesse').empty();
-                result.forEach(commessa => addCommessaToList(commessa));
-            });
-        };
-    });
-
-    /*
-    form.submit((event) => {
-        let nomeArticolo = $("#nome-articolo").val();
-        let startDate = $("#dtp-inizio").val();
-        let endDate = $("#dtp-fine").val();
         
-        console.log(nomeArticolo)
-        console.log(startDate)
-        console.log(endDate)
+        event.preventDefault();
 
         if(!nomeArticolo && !startDate && !endDate) {
-            event.preventDefault();
             fetchAllData();
         };
         if(!nomeArticolo) {
-            event.preventDefault();
             $.ajax({
                 type: 'GET',
                 url: `http://localhost:3000/api/history?from=${startDate}&to=${endDate}`,
@@ -77,7 +27,6 @@ const fetchFilteredData = () => {
             });
         }
         if(!startDate && !endDate) {
-            event.preventDefault();
             $.ajax({
                 type: 'GET',
                 url: `http://localhost:3000/api/history?articolo=${nomeArticolo}`,
@@ -87,7 +36,6 @@ const fetchFilteredData = () => {
             });
         }
         if(nomeArticolo && startDate && endDate) {
-            event.preventDefault();
             $.ajax({
                 type: 'GET',
                 url: `http://localhost:3000/api/history?articolo=${nomeArticolo}&from=${startDate}&to=${endDate}`,
@@ -97,7 +45,6 @@ const fetchFilteredData = () => {
             });
         };
     });
-    */
 };
 
 const fetchAllData = () => {
@@ -108,20 +55,21 @@ const fetchAllData = () => {
         $('#accordion-commesse').empty();
         result.forEach(commessa => addCommessaToList(commessa));
     });
-}
+};
 
 const addCommessaToList = (commessa) => {
     const template = $(`
-        <div class="card bg-dark">
+        <div class="card lighter-back border-lighter">
             <div class="card-header" role="tab" id="heading_${commessa._id}">
                 <a data-toggle="collapse" data-parent="#accordion-commesse" href="#collapse_${commessa._id}" aria-expanded="false" aria-controls="collapse_${commessa._id}">
                     <div class="row">
                         <div class="col-md-auto"></div>
-                        <div class="col h4 text-light mt-2">${commessa.codice_commessa}<i class="fas fa-angle-down rotate-icon"></i></div>        
+                        <div class="col-lg-8 h4 text-light mt-2">${commessa.codice_commessa}</div>
+                        <div class="col-md-auto text-light mt-2">${dateFormat(commessa.data_consegna)}</div>       
                     </div>
                 </a>
             </div>
-            <div id="collapse_${commessa._id}" class="collapse bg-light text-dark" role="tabpanel" aria-labelledby="heading_${commessa._id}" data-parent="accordion-commesse">
+            <div id="collapse_${commessa._id}" class="collapse dark-back" role="tabpanel" aria-labelledby="heading_${commessa._id}" data-parent="accordion-commesse">
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-auto"></div>
