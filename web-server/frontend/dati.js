f46685dc8628a8d375686f786906657616ee1f40
yungfox <crifox12@gmail.com>
2021-05-19 12:15:06 +0200

    spostato metodi grafico su dati.js

--- web-server/frontend/dati.js
+++ web-server/frontend/dati.js
@@ -35,6 +35,7 @@ $(document).ready(function () {
 //const baseURL = 'http://localhost:3000/api/';
 const baseURL = 'http://54.85.250.76:3000/api/';
 let nomeArticolo;
+let graphData;
 
 const fetchFilteredData = () => {
     nomeArticolo = document.getElementById("nome-articolo").value;
@@ -47,78 +48,7 @@ const fetchFilteredData = () => {
 
     event.preventDefault();
 
-    /* if (!nomeArticolo && !startDate && !endDate) {
-        $.ajax({
-            type: 'GET',
-            url: `${baseURL}history`,
-        }).then(result => {
-            $('#accordion-commesse').empty();
-            result.forEach(commessa => {
-                if ($('#switch-completato').is(":checked") === true) {
-                    if (commessa.stato === 'fallita') {
-                        addCommessaToList(commessa)
-                    }
-                } else {
-                    addCommessaToList(commessa)
-                }
-            });
-            loadTable();
-        });
-    };
-    if (!nomeArticolo) {
-        $.ajax({
-            type: 'GET',
-            url: `${baseURL}history?from=${startDate}&to=${endDate}`,
-        }).then(result => {
-            $('#accordion-commesse').empty();
-            result.forEach(commessa => {
-                if ($('#switch-completato').is(":checked") === true) {
-                    if (commessa.stato === 'fallita') {
-                        addCommessaToList(commessa)
-                    }
-                } else {
-                    addCommessaToList(commessa)
-                }
-            });
-            loadTable();
-        });
-    }
-    if (!startDate && !endDate) {
-        $.ajax({
-            type: 'GET',
-            url: `${baseURL}history?articolo=${nomeArticolo}`,
-        }).then(result => {
-            $('#accordion-commesse').empty();
-            result.forEach(commessa => {
-                if ($('#switch-completato').is(":checked") === true) {
-                    if (commessa.stato === 'fallita') {
-                        addCommessaToList(commessa)
-                    }
-                } else {
-                    addCommessaToList(commessa)
-                }
-            });
-            loadTable();
-        });
-    }
-    if (nomeArticolo && startDate && endDate) {
-        $.ajax({
-            type: 'GET',
-            url: `${baseURL}history?articolo=${nomeArticolo}&from=${startDate}&to=${endDate}`,
-        }).then(result => {
-            $('#accordion-commesse').empty();
-            result.forEach(commessa => {
-                if ($('#switch-completato').is(":checked") === true) {
-                    if (commessa.stato === 'fallita') {
-                        addCommessaToList(commessa)
-                    }
-                } else {
-                    addCommessaToList(commessa)
-                }
-            });
-            loadTable();
-        });
-    }; */
+    
 
     $.ajax({
         type: 'GET',
@@ -134,8 +64,8 @@ const fetchFilteredData = () => {
                 addCommessaToList(commessa)
             }
         });
-        loadTable();
         document.getElementById("nome-articolo").value = nomeArticolo;
+        loadTable();
     });
 };
 
@@ -203,6 +133,8 @@ const fetchAllData = () => {
             datiLavorazione(result);
             datiProgresso(result);
             loadTable();
+            graphData = formatGraphData(result.history);
+            renderGraph();
         });
 };
 
@@ -337,3 +269,192 @@ const dateFormat = (date) => {
     let formattedDate = `${giorni[newDate.getDay()]} ${("0" + newDate.getDate()).slice(-2)} ${mesi[newDate.getMonth()]} ${newDate.getFullYear()} ${("0" + newDate.getUTCHours()).slice(-2)}:${("0" + newDate.getUTCMinutes()).slice(-2)}:${("0" + newDate.getUTCSeconds()).slice(-2)}`
     return formattedDate;
 }
+
+const formatGraphData = (dati) => {
+    let formattedGraphData = [];
+
+    for (const x of dati) {
+        let data = formattedGraphData[graphDateFormat(x.data_consegna)];
+        let pezziTotali = x.quantita_prodotta;
+        let pezziScartati = x.quantita_scarto_difettoso + x.quantita_scarto_pieno;
+
+        if (data == undefined) {
+            data = {
+                "pezzi_totali": pezziTotali,
+                "pezzi_scartati": pezziScartati
+            }
+        } else {
+            data = {
+                "pezzi_totali": pezziTotali + data.pezzi_totali,
+                "pezzi_scartati": pezziScartati + data.pezzi_scartati
+            }
+        }
+        formattedGraphData[graphDateFormat(x.data_consegna)] = data;
+    }
+
+    let data_x = [];
+    let pezziTotaliGiornalieri = [];
+    let pezziScartatiGiornalieri = [];
+
+    Object.keys(formattedGraphData).forEach(function (key) {
+        data_x.push(key);
+        pezziTotaliGiornalieri.push(formattedGraphData[key].pezzi_totali);
+        pezziScartatiGiornalieri.push(formattedGraphData[key].pezzi_scartati);
+    });
+
+    return [data_x, pezziTotaliGiornalieri, pezziScartatiGiornalieri];
+}
+
+const graphDateFormat = (rawDate) => {
+    let newDate = new Date(rawDate);
+    let formattedDate = `${newDate.getFullYear()}-${("0" + (newDate.getMonth() + 1)).slice(-2)}-${("0" + newDate.getDate()).slice(-2)}`
+    return formattedDate;
+}
+
+const renderGraph = () => {
+    var optionsLine = {
+        chart: {
+            width: "95%",
+            foreColor: "#f8f9fa",
+            background: "#0e0831",
+            toolbar: {
+                show: true
+            },
+            height: "200%",
+            type: 'line',
+            zoom: {
+                enabled: true
+            },
+            dropShadow: {
+                enabled: true,
+                top: 3,
+                left: 2,
+                blur: 4,
+                opacity: 1,
+            }
+        },
+        tooltip: {
+            theme: "dark"
+        },
+        stroke: {
+            curve: 'smooth',
+            width: 2
+        },
+        colors: ["#00F790", '#F7AA00'],
+        series: [{
+            name: "pezzi prodotti",
+            data: graphData[1]
+        },
+        {
+            name: "pezzi scartati",
+            data: graphData[2]
+        }
+        ],
+        markers: {
+            size: 6,
+            strokeWidth: 0,
+            hover: {
+                size: 9
+            }
+        },
+        grid: {
+            show: true,
+            padding: {
+                bottom: 0
+            }
+        },
+        labels: graphData[0],
+        xaxis: {
+            tooltip: {
+                enabled: false
+            }
+        },
+        legend: {
+            position: 'bottom',
+            horizontalAlign: 'center'
+        },
+        responsive: [
+            {
+                breakpoint: 1000,
+                options: {
+                }
+            }
+        ]
+    }
+
+    var chartLine = new ApexCharts(document.querySelector('#graph'), optionsLine);
+    chartLine.render();
+}
+
+/* if (!nomeArticolo && !startDate && !endDate) {
+        $.ajax({
+            type: 'GET',
+            url: `${baseURL}history`,
+        }).then(result => {
+            $('#accordion-commesse').empty();
+            result.forEach(commessa => {
+                if ($('#switch-completato').is(":checked") === true) {
+                    if (commessa.stato === 'fallita') {
+                        addCommessaToList(commessa)
+                    }
+                } else {
+                    addCommessaToList(commessa)
+                }
+            });
+            loadTable();
+        });
+    };
+    if (!nomeArticolo) {
+        $.ajax({
+            type: 'GET',
+            url: `${baseURL}history?from=${startDate}&to=${endDate}`,
+        }).then(result => {
+            $('#accordion-commesse').empty();
+            result.forEach(commessa => {
+                if ($('#switch-completato').is(":checked") === true) {
+                    if (commessa.stato === 'fallita') {
+                        addCommessaToList(commessa)
+                    }
+                } else {
+                    addCommessaToList(commessa)
+                }
+            });
+            loadTable();
+        });
+    }
+    if (!startDate && !endDate) {
+        $.ajax({
+            type: 'GET',
+            url: `${baseURL}history?articolo=${nomeArticolo}`,
+        }).then(result => {
+            $('#accordion-commesse').empty();
+            result.forEach(commessa => {
+                if ($('#switch-completato').is(":checked") === true) {
+                    if (commessa.stato === 'fallita') {
+                        addCommessaToList(commessa)
+                    }
+                } else {
+                    addCommessaToList(commessa)
+                }
+            });
+            loadTable();
+        });
+    }
+    if (nomeArticolo && startDate && endDate) {
+        $.ajax({
+            type: 'GET',
+            url: `${baseURL}history?articolo=${nomeArticolo}&from=${startDate}&to=${endDate}`,
+        }).then(result => {
+            $('#accordion-commesse').empty();
+            result.forEach(commessa => {
+                if ($('#switch-completato').is(":checked") === true) {
+                    if (commessa.stato === 'fallita') {
+                        addCommessaToList(commessa)
+                    }
+                } else {
+                    addCommessaToList(commessa)
+                }
+            });
+            loadTable();
+        });
+    }; */
