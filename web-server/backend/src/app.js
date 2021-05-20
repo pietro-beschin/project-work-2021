35fdeae753f9bef197caf607d0aec0d1be77f924
pietro-beschin <pietro.beschin@stud.tecnicosuperiorekennedy.it>
2021-05-20 12:34:03 +0200

    test 2 https

--- web-server/backend/src/app.js
+++ web-server/backend/src/app.js
@@ -9,8 +9,8 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-    key : fs.readFileSync('/opt/bitnami/apache2/conf/bitnami/certs/server.key'),
-    cert : fs.readFileSync('/opt/bitnami/apache2/conf/bitnami/certs/server.crt')
+    key : fs.readFileSync('/opt/bitnami/apache2/conf/privkey.pem'),
+    cert : fs.readFileSync('/opt/bitnami/apache2/conf/bitnami/certs/key-cert.pem')
 };
 mongoose.connect("mongodb://10.0.25.202:27017/project-work-2021", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('debug', true);
