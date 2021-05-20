1930840425155d6f6409d60a20515edfb439d34f
pietro-beschin <pietro.beschin@stud.tecnicosuperiorekennedy.it>
2021-05-20 09:11:06 +0200

    test partial match

--- web-server/backend/src/api/history/history.model.js
+++ web-server/backend/src/api/history/history.model.js
@@ -18,7 +18,7 @@ module.exports.list = async (query) => {
         if(query.hideCompleted === 'true'){
             q.$where = "Number(this.quantita_prodotta) < Number(this.quantita_prevista)";
         }
-        return await historySchema.find(q, {articolo: regExpArticolo(query.articolo)});
+        return await historySchema.find(q, {articolo: newRegExp(`.*${query.articolo}.*`, 'i')});
 }
 
 module.exports.store = async (data) => {  
